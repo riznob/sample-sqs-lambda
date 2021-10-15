afa889734e3501f268fbe220138ff34340ab03a1
Rob Anderson <riznob@gmail.com>
2021-10-15 15:43:11 -0600

    make region a variable

--- scripts/send-message.sh
+++ scripts/send-message.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+REGION=
 URL=
 
-aws sqs send-message --queue-url $URL --message-body "Information about the largest city in Any Region." --delay-seconds 10 --message-attributes file://send-message.json
+aws --region $REGION sqs send-message --queue-url $URL --message-body "Information about the largest city in Any Region." --delay-seconds 10 --message-attributes file://send-message.json
